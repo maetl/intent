712303136dfc96ce6e0b6f3fff04a54dd3a164ce
Mark Rickerby <me@maetl.net>
2016-02-28 02:12:08 +1100

    Add focus task
    This is an extraordinarily messy and quick hack, but the value I get out
    of randomly being able to flip through a backlog of focused tasks far
    outweighs the embarassment of such shitty code being public.

--- lib/intent/todo/manager.rb
+++ lib/intent/todo/manager.rb
@@ -11,6 +11,24 @@ module Intent
             list.each do |task|
               puts task
             end
+          when :focus
+            unless args[1].nil?
+              case args[1][0]
+              when '@'
+                focused_list = list.by_context(args[1])
+              when '+'
+                focused_list = list.by_project(args[1])
+              else
+                focused_list = list.by_context(args[1]).by_project(args[1])
+              end
+            end
+
+            prioritised_list = focused_list.by_priority('A')
+            if prioritised_list.any?
+              puts prioritised_list.by_not_done.sample
+            else
+              puts focused_list.by_not_done.sample
+            end
           end
         end
       end
