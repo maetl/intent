9951a43171f28ea8625bc36cfa6dc8c1db58bbc3
Mark Rickerby <me@maetl.net>
2018-07-02 19:39:21 +1200

    Conditionally highlight todo list items if stdout is a tty
    This ensures the command line tool doesn’t include formatting codes in
    the output when piped to another process or redirected to a file.
    
    The only command that currently supports this is `todo list`.

--- lib/gem_ext/todo-txt.rb
+++ lib/gem_ext/todo-txt.rb
@@ -1,10 +1,16 @@
 # Monkeypatch `todo-txt-gem` to create ANSI decorated terminal output.
 class Todo::Task
   def to_s_highlighted
+    # If stdout is not a tty, then return the raw string as the output is
+    # probably being piped to a file and the color codes will make a mess.
+    return to_s unless STDOUT.tty?
+
     pastel = Pastel.new
     if done?
+      # Completed tasks are rendered with a strikethrough
       pastel.strikethrough(to_s)
     else
+      # Open tasks delegate to the custom formatting function
       print_open_task(pastel)
     end
   end
