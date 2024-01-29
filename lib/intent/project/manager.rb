877962edbef192174f099cfd5b63fb5976bfa1fd
Mark Rickerby <me@maetl.net>
2024-01-29 23:49:30 +1300

    Sketch of project tasks

---
+++ lib/intent/project/manager.rb
@@ -0,0 +1,22 @@
+module Intent
+  module Project
+    class Manager < Intent::CommandContext
+      def run(args, output=STDOUT)
+        if args.empty?
+          print_help(output)
+        else
+          case args.first.to_sym
+          when :help
+            print_help(output)
+          when :status
+            Status.run(File.expand_path('~/Projects').to_s)
+          when :focus
+            # Focus.run()
+          when :sync
+            # run_sync( Sync.instance )
+          end
+        end
+      end
+    end
+  end
+end
